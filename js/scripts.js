fe46a08912da3f1bbd34bef0db21a774ac83ff82
hyolmotsheringdorje <hyolmotsheringdorje@gmail.com>
2026-09-02 13:46:44 +0900

    test

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+73% of storage used … If you run out, you can't create, edit, and upload files. Share 100 GB of storage with your family members for ¥15 for 1 month ¥290.
+100%
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
